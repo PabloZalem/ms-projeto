bbd066f5f6708a338efe947b57ab9e53fceb12f8
PabloZalem <zalem.pablo@yahoo.com>
2023-10-11 17:11:26 -0300

    Rest Template

---
+++ folhaDePagamento/src/main/java/com/zalempablo/project/folhadepagamento/entities/Trabalhador.java
@@ -0,0 +1,65 @@
+package com.zalempablo.project.folhadepagamento.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Trabalhador implements Serializable {
+
+	private static final long serialVersionUID = 1L; //Transformar a classe em bytes
+	
+	private Long id;
+	private String nome;
+	private Double ganhaPorDia;
+
+	public Trabalhador() {
+	}
+
+	public Trabalhador(Long id, String nome, Double ganhaPorDia) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.ganhaPorDia = ganhaPorDia;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getGanhaPorDia() {
+		return ganhaPorDia;
+	}
+
+	public void setGanhaPorDia(Double ganhaPorDia) {
+		this.ganhaPorDia = ganhaPorDia;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trabalhador other = (Trabalhador) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- folhaDePagamento/src/main/java/com/zalempablo/project/folhadepagamento/service/FolhaDePagamentoService.java
+++ folhaDePagamento/src/main/java/com/zalempablo/project/folhadepagamento/service/FolhaDePagamentoService.java
@@ -1,13 +1,30 @@
 package com.zalempablo.project.folhadepagamento.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.zalempablo.project.folhadepagamento.entities.FolhaDePagamento;
+import com.zalempablo.project.folhadepagamento.entities.Trabalhador;
 
 @Service
 public class FolhaDePagamentoService {
 	
+	@Value("${trabalhador.host}")
+	private String trabalhador;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public FolhaDePagamento getPagamento(long workerId, int dias) {
-		return new FolhaDePagamento("Bob",200.0, dias);
+		Map<String, String> variaveiesUri = new HashMap<>();
+		variaveiesUri.put("id", ""+workerId);
+		
+		Trabalhador trabalhadores = restTemplate.getForObject(trabalhador+"/trabalhadores/{id}",Trabalhador.class, variaveiesUri);
+		return new FolhaDePagamento(trabalhadores.getNome(),trabalhadores.getGanhaPorDia(), dias);
 	}
 }
