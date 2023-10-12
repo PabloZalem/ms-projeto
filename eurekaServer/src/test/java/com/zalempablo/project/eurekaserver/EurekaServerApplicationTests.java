f6f24fa2471e0378c01bb79cf155461f926284fd
PabloZalem <zalem.pablo@yahoo.com>
2023-10-12 19:09:27 -0300

    Subindo o eureka server

---
+++ eurekaServer/src/test/java/com/zalempablo/project/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalempablo.project.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
